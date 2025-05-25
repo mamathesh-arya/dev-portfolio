a9b4a4dd1fef853246c2f7f29d148c32dac4485a
mamathesh-arya <mamathesh333@gmail.com>
2025-05-25 12:18:37 +0530

    changes3

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/dev-portfolio/",
   server: {
     host: "::",
     port: 8080,
@@ -19,5 +18,10 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  base:"/dev-portfolio/",
+  build:{
+    outDir: 'dist', 
+
   },
 }));
